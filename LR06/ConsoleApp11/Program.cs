8ea350afadd7ac2cc1dacecfe4f8ee9af0c96a4a
lindows310 <161236979+lindows310@users.noreply.github.com>
2025-11-14 23:13:54 +0400

    -
    -

---
+++ LR06/ConsoleApp11/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа No 6. Выполнил студент группы 6101-020302D Абросимов Артём");
+            Console.Write("Введите строку: ");
+
+            string str = Console.ReadLine();
+            StringAnalyzer analyzer = new StringAnalyzer(str);
+
+            bool run;
+            run = true;
+
+            while (run)
+            {
+                string input;
+
+                Console.WriteLine("\nВведите слово:\n(1) Вывести информацию о количестве символов; подсчитать среднюю длину слова\n(2) Заменить вхождения указанного слова новым словом\n(3) Подсчитать количество вхождений подстроки\n(4) Проверка на палиндром\n(5) Проверка на дату\n(6) Вывести текущую строку\n(7) Выход из программы");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case ("1"):
+                        Console.WriteLine($"Количество букв: {analyzer.letCount}\nКоличество цифр: {analyzer.numCount}\nСредняя длина слова: {analyzer.averageLength}");
+                        break;
+                    case ("2"):
+                        Console.WriteLine("Введите слово, которое хотите заменить, и слово, которым вы хотите заменить вхождения:");
+                        string[] inp = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        analyzer.ReplaceWith(inp[0], inp[1]);
+                        break;
+                    case ("3"):
+                        Console.WriteLine("Введите подстроку, количество вхождений которой вы хотите высчитать:");
+                        string userInp = Console.ReadLine();
+                        Console.WriteLine($"Количество вхождений указанной подстроки: {analyzer.SubstringEntries(userInp)}");
+                        break;
+                    case ("4"):
+                        string isPalindrom = analyzer.PalindromCheck() == true ? "Строка является палиндромом" : "Строка не является палиндромом";
+                        Console.WriteLine(isPalindrom);
+                        break;
+                    case ("5"):
+                        string isDate = analyzer.DateCheck() == true ? "Строка является датой" : "Строка не является датой";
+                        Console.WriteLine(isDate);
+                        break;
+                    case ("6"):
+                        Console.WriteLine($"Текущая строка:\n{analyzer.Str}");
+                        break;
+                    case ("7"):
+                        Console.WriteLine("Вы вышли из программы.");
+                        run = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ LR06/ConsoleApp11/StringAnalyzer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class StringAnalyzer
+    {
+        static string numsAlphabet = "0123456789";
+        static string kirillicAlphabet = "йцукенгшщзхъфывапролджэячсмитьбю";
+        static string latAlphabet = "qwertyuiopasdfghjklzxcvbnm";
+        static string alphabet = kirillicAlphabet + kirillicAlphabet.ToUpper() + latAlphabet + latAlphabet.ToUpper();
+
+        string str;
+        public int numCount { get; set; }
+        public int letCount { get; set; }
+        public double averageLength { get; set; }
+        public string Str
+        {
+            get { return str; }
+            set { str = value ?? "None"; }
+        }
+        public StringAnalyzer()
+        {
+            Str = "None";
+            numCount = letCount = 0;
+        }
+        public StringAnalyzer(string str)
+        {
+            Str = str;
+            NumLetCount();
+            AverageLength();
+        }
+        private void NumLetCount()
+        {
+            if (str != "None")
+            {
+                string tempStr = str.Replace(" ", "");
+                for (int i = 0; i < tempStr.Length; i++)
+                {
+                    if (numsAlphabet.Contains(tempStr[i]))
+                        numCount++;
+                    else if (alphabet.Contains(tempStr[i]))
+                        letCount++;
+                    else
+                        continue;
+                }
+            }
+        }
+        private void AverageLength()
+        {
+            if (str != "None")
+            {
+                string tempStr = string.Empty;
+                for (int i = 0; i < str.Length; i++)
+                    if (alphabet.Contains(str[i]) || str[i] == ' ')
+                        tempStr += str[i];
+
+                int test1 = tempStr.Replace(" ", "").Length;
+                int test2 = tempStr.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Count();
+
+                averageLength = ( (double) tempStr.Replace(" ", "").Length / tempStr.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Count());
+            }
+        }
+        public void ReplaceWith(string word, string wordReplace)
+        {
+            string[] tempStr = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < tempStr.Count(); i++)
+                if (tempStr[i].Substring(1).ToLower() == word.Substring(1).ToLower() && tempStr[i].Substring(2) == word.Substring(2))
+                    str = str.Replace(tempStr[i], wordReplace);
+        }
+        public int SubstringEntries(string substring)
+        {
+            int entriesCount = 0;
+            string temp;
+            string[] tempStr = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < tempStr.Count(); i++)
+            {
+                temp = tempStr[i];
+                while (temp.IndexOf(substring) != -1)
+                {
+                    entriesCount++;
+                    temp = temp.Substring(temp.IndexOf(substring) + 1);
+                }
+            }
+            return entriesCount;
+        }
+        public bool PalindromCheck()
+        {
+            string strTrim1 = str.Substring(0, (int) Math.Ceiling(a: str.Length / 2) + 1);
+            string strTrim2 = str.Substring( (int) Math.Ceiling(a: str.Length / 2));
+
+            string trim2Reversed = new string(strTrim2.Reverse().ToArray());
+            bool isTrue = String.Compare(strTrim1, trim2Reversed) == 0 ? true : false;
+            return isTrue;
+        }
+        public bool DateCheck()
+        {
+            bool StrIsData;
+            string[] dateComponent = str.Split('.');
+            if (int.TryParse(dateComponent[0], out int d1) == true && int.TryParse(dateComponent[1], out int d2) == true && int.TryParse(dateComponent[2], out int d3) == true)
+                StrIsData = (d1 > 0 && d1 < 32) && (d2 > 0 && d2 < 13);
+            else
+                StrIsData = false;
+            return StrIsData;
+        }
+    }
+}
